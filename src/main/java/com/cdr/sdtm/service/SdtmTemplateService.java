f66b2d25c91760b70e9ae3d9aa1efd9048e52a78
hchitta <haritha.chitta@gmail.com>
2018-10-26 18:33:33 +0530

    methods added

--- src/main/java/com/cdr/sdtm/service/SdtmTemplateService.java
+++ src/main/java/com/cdr/sdtm/service/SdtmTemplateService.java
@@ -1,5 +1,11 @@
 package com.cdr.sdtm.service;
 
+import java.util.List;
+
+import com.cdr.sdtm.model.PathToSdtmTemplate;
+
 public interface SdtmTemplateService {
+
+	List<PathToSdtmTemplate> findByDomain(String domain);
 
 }
--- src/main/java/com/cdr/sdtm/service/SdtmTemplateServiceImpl.java
+++ src/main/java/com/cdr/sdtm/service/SdtmTemplateServiceImpl.java
@@ -1,8 +1,11 @@
 package com.cdr.sdtm.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cdr.sdtm.model.PathToSdtmTemplate;
 import com.cdr.sdtm.repository.SdtmTemplateRepository;
 
 @Service
@@ -10,5 +13,11 @@ public class SdtmTemplateServiceImpl implements SdtmTemplateService {
 	
 	@Autowired
 	SdtmTemplateRepository sdtmTemplateRepository;
+
+	@Override
+	public List<PathToSdtmTemplate> findByDomain(String domain) {
+		
+		return sdtmTemplateRepository.findByDomain(domain);
+	}
 
 }
