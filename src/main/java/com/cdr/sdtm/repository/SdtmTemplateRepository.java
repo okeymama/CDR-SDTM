02a05d76aae3c0fcb11dd65ded424297a3127054
hchitta <haritha.chitta@gmail.com>
2018-10-26 17:54:25 +0530

    added new method

--- src/main/java/com/cdr/sdtm/repository/SdtmTemplateRepository.java
+++ src/main/java/com/cdr/sdtm/repository/SdtmTemplateRepository.java
@@ -1,9 +1,13 @@
 package com.cdr.sdtm.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cdr.sdtm.model.PathToSdtmTemplate;
 
 public interface SdtmTemplateRepository extends JpaRepository<PathToSdtmTemplate, Long>{
+
+	List<PathToSdtmTemplate> findByDomain(String domain);
 
 }
