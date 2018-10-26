a7e3c00405bd46b5debd36fe0785e17e2cc9e730
hchitta <haritha.chitta@gmail.com>
2018-10-26 18:34:27 +0530

    method added

--- src/main/java/com/cdr/sdtm/controller/SdtmTemplateController.java
+++ src/main/java/com/cdr/sdtm/controller/SdtmTemplateController.java
@@ -1,21 +1,35 @@
 package com.cdr.sdtm.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cdr.sdtm.model.PathToSdtmTemplate;
 import com.cdr.sdtm.service.SdtmTemplateService;
 
 
 @RestController
-@RequestMapping("/api/template")
+@RequestMapping("/api/CDR")
 public class SdtmTemplateController {
 	
 private static final Logger LOGGER = LoggerFactory.getLogger(StudyController.class);
 	
 	@Autowired
 	SdtmTemplateService sdtmTemplateService;
+	
+	@GetMapping("/template/{domain}")
+	public List<PathToSdtmTemplate> findByDomain(@PathVariable String domain) {
+		LOGGER.info("Method Starts requested template for domain " + domain);
+		List<PathToSdtmTemplate> localTemp = sdtmTemplateService.findByDomain(domain);
+		LOGGER.info("Method Ends.");
+		return localTemp;
+		
+	}
 
 }
