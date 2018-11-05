3c7041c0c8b3ecf378a06a85319e5915e57cf9a1
hchitta <haritha.chitta@gmail.com>
2018-11-05 16:29:52 +0530

    distinct study title changes

--- src/main/java/com/cdr/sdtm/repository/StudyRepository.java
+++ src/main/java/com/cdr/sdtm/repository/StudyRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +16,8 @@ public interface StudyRepository extends JpaRepository<Study, Integer>, QueryByE
 	List<Study> findByStudyID(String studyID);
 	
 	List<Study> findAll(Example exp);
+	
+	@Query("select distinct title from STUDY where title is not null")
+	List<String> findDistinctTitles();
 	
 }
