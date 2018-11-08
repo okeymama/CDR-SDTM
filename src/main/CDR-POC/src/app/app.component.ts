daf24d5ff39a1114681a18c183e05e83f63c9ec3
ermamtha <mamramaswamy@deloitte.com>
2018-11-08 22:05:16 +0530

    UX changes

--- src/main/CDR-POC/src/app/app.component.ts
+++ src/main/CDR-POC/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject  } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators/map';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  paramId: string;
+  route: string;
+  Login: boolean;
+
+  constructor(location: Location, router: Router) {
+    router.events.subscribe((val) => {
+      if(location.path() != ''){
+        this.route = location.path();
+      } else {
+        this.Login = true;
+      }
+    });
+  }
 }
