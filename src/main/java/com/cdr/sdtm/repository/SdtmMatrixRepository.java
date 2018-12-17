ae0524908a580eeecaafddc44907564a9339014e
hchitta <haritha.chitta@gmail.com>
2018-12-17 13:08:59 +0530

    query change

--- src/main/java/com/cdr/sdtm/repository/SdtmMatrixRepository.java
+++ src/main/java/com/cdr/sdtm/repository/SdtmMatrixRepository.java
@@ -36,7 +36,7 @@ public interface SdtmMatrixRepository extends JpaRepository<PathToSdtmMatrix, Lo
 			" FROM SDTM.path_to_sdtm_matrix_2 a " + 
 			" WHERE Form_Label <> ''" + 
 			" AND Study_Title=:study and Domain_Name=:domain " + 
-			" GROUP BY Domain_Label,CONCAT(Form_Label, ' (', Domain_Name, ') ')" + 
+			" GROUP BY Domain_Label,CONCAT(Form_Label, ' (', Form_Name, ') ')" + 
 			" ORDER BY MAX(Join_Criteria)  ASC")
 	List<Object[]> fetchObjectLevelByStudyAndDomain(@Param("study") String study,@Param("domain") String domain);
 
